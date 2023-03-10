4dea64f4ab0076796bc467deb0bc02d533f4a2b7
nguthanhduong <ngu.duong@zodinet.tech>
2023-03-10 14:33:05 +0700

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NotificationModule } from './notification/notification.module';
+import { BlackListModule } from './black-list/black-list.module';
 
 @Module({
-  imports: [],
+  imports: [NotificationModule, BlackListModule],
   controllers: [AppController],
   providers: [AppService],
 })
