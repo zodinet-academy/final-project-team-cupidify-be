87aa81261eb2fb852822c6664661c641380cfc07
nguthanhduong <ngu.duong@zodinet.tech>
2023-03-16 10:10:41 +0700

    test: google login api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,18 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  UseGuards,
+  Req,
+  HttpStatus,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dto/sign-up.dto';
 import { FirebaseTokenDto } from './dto/firebase-token.dto';
 import { LogInDto } from './dto/log-in.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { GoogleTokenDto } from './dto/gg-token.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -12,6 +22,18 @@ export class AuthController {
   // checkPhoneExist(@Body() checkPhoneDto: CheckPhoneDto) {
   //   return this._authService.isPhoneExist(checkPhoneDto);
   // }
+
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  async googleAuth(@Req() req) {
+    return HttpStatus.OK;
+  }
+
+  @Get('google/redirect')
+  @UseGuards(AuthGuard('google'))
+  googleAuthRedirect(@Req() req) {
+    return this._authService.googleLogin(req);
+  }
 
   @Post('signup')
   signUp(@Body() signUpDto: SignUpDto) {
@@ -23,8 +45,13 @@ export class AuthController {
     return this._authService.login(logInDto);
   }
 
-  @Post('verify-token')
-  verifyToken(@Body() firebaseTokenDto: FirebaseTokenDto) {
+  @Post('verify-otp-token')
+  verifyOtpToken(@Body() firebaseTokenDto: FirebaseTokenDto) {
     return this._authService.verifyFirebaseToken(firebaseTokenDto.token);
+  }
+
+  @Post('verify-gg-token')
+  verifyGgToken(@Body() ggTokenDto: GoogleTokenDto) {
+    return this._authService.verifyGgToken(ggTokenDto.token);
   }
 }
---
+++ src/auth/dto/gg-token.dto.ts
@@ -0,0 +1,3 @@
+export class GoogleTokenDto {
+  token: string;
+}
