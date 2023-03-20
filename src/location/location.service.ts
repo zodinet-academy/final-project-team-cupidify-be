312d829081f0524b935b34940625f31b85a95673
nguthanhduong <ngu.duong@zodinet.tech>
2023-03-20 16:43:49 +0700

    wip: location

--- src/location/location.service.ts
+++ src/location/location.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateLocationDto } from './dto/create-location.dto';
 import { UpdateLocationDto } from './dto/update-location.dto';
+import { Location } from './entities/location.entity';
 
 @Injectable()
 export class LocationService {
+  constructor(
+    @InjectRepository(Location)
+    private readonly _locationRepository: Repository<Location>,
+  ) {}
+
   create(createLocationDto: CreateLocationDto) {
     return 'This action adds a new location';
   }
