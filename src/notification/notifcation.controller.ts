e3a81bffe9f95c51f56b59ba6ebdaa0fc3a4d618
danh <thdanh2001@gmail.com>
2023-04-17 14:47:12 +0700

    Merge branch 'main' into dev
    # Conflicts:
    #	src/auth/guards/gateway.guard.ts
    #	src/conversation/conversation.controller.ts
    #	src/conversation/conversation.module.ts
    #	src/conversation/conversation.profile.ts
    #	src/conversation/conversation.service.ts
    #	src/message/dto/create-message.dto.ts
    #	src/message/message.controller.ts
    #	src/message/message.gateway.ts
    #	src/message/message.module.ts
    #	src/message/message.service.ts
    #	src/notification/notification.service.ts
    #	src/profile/dto/profile-conversation.dto.ts
    #	src/profile/interfaces/index.ts
    #	src/profile/profile.profile.ts
    #	src/profile/profile.service.ts

--- src/notification/notifcation.controller.ts
+++ src/notification/notifcation.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Put, Query, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { AuthenticationGuard } from 'src/auth/guards/auth.guard';
 import { User } from 'src/user/decorator/user.decorator';
@@ -16,7 +16,35 @@ export class NotificationController {
   @ApiBearerAuth()
   @UseGuards(AuthenticationGuard)
   @Get()
-  getNotifications(@User() user: UserDto) {
-    return this._notificationService.totalNotificationByUser(user.id);
+  getNotifications(
+    @User() user: UserDto,
+    @Query('page') page = 1,
+    @Query('limit') limit = 3,
+  ) {
+    return this._notificationService.totalNotificationByUser(
+      user.id,
+      page,
+      limit,
+    );
+  }
+
+  @ApiOkResponse({
+    description: 'Update notification sucessfully!',
+  })
+  @ApiBearerAuth()
+  @UseGuards(AuthenticationGuard)
+  @Put()
+  updateRead(@Body() updateNotiDto) {
+    return this._notificationService.updateNotiRead(updateNotiDto);
+  }
+
+  @ApiOkResponse({
+    description: 'Mark all notifications read',
+  })
+  @ApiBearerAuth()
+  @UseGuards(AuthenticationGuard)
+  @Put('markAllRead')
+  updateAllRead(@User() user: UserDto) {
+    return this._notificationService.markAllRead(user.id);
   }
 }
